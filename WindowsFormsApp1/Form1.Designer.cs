5d1128f716c35c367f0764736efb86177df9bdeb
大石头 <nnhy@vip.qq.com>
2019-05-09 12:09:09 +0800

    可以浏览页面

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lbStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1877, 1084);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(47, 22);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1136, 39);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://www.chinacar.com.cn/search.html";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(1223, 13);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(111, 52);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "转到";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lbStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1167);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1901, 36);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(62, 33);
+            this.lbStatus.Text = "就绪";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1901, 1203);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lbStatus;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using NewLife.Log;
+
+namespace WindowsFormsApp1
+{
+    [ComVisible(true)]
+    public partial class Form1 : Form
+    {
+        public Uri Home { get; set; }
+
+        private ChromiumWebBrowser Browser { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitBrowser();
+        }
+
+        public void InitBrowser()
+        {
+            var settines = new CefSettings()
+            {
+                Locale = "zh-CN",
+                AcceptLanguageList = "zh-CN",
+                MultiThreadedMessageLoop = true
+            };
+
+            Cef.Initialize(settines);
+
+            var bw = new ChromiumWebBrowser("");
+            panel1.Controls.Add(bw);
+            bw.Dock = DockStyle.Fill;
+
+            bw.FrameLoadStart += Browser_FrameLoadStart;
+            bw.FrameLoadEnd += Web_FrameLoadEnd;
+            bw.StatusMessage += Bw_StatusMessage;
+            bw.TitleChanged += Bw_TitleChanged;
+
+            Browser = bw;
+        }
+
+        private void Bw_TitleChanged(Object sender, TitleChangedEventArgs e)
+        {
+            this.Invoke(() => Text = e.Title);
+        }
+
+        private void Bw_StatusMessage(Object sender, StatusMessageEventArgs e)
+        {
+            this.Invoke(() => lbStatus.Text = e.Value);
+        }
+
+        private void Form1_Load(Object sender, EventArgs e)
+        {
+            //var url = "http://www.chinacar.com.cn/Home/GonggaoSearch/GonggaoSearch/search_json?_dc=" + DateTime.Now.Ticks;
+
+            //var dic = new Dictionary<String, String>();
+            //dic["s7"] = "YCS04200-68";
+
+            //var client = new HttpClient();
+            //var rs = client.PostAsync(url, new FormUrlEncodedContent(dic)).Result;
+            //var html = rs.Content.ReadAsStringAsync().Result;
+
+            var url = "http://www.chinacar.com.cn/search.html";
+            Home = new Uri(url);
+
+            //Browser.Load(url);
+        }
+
+        private void Browser_FrameLoadStart(Object sender, FrameLoadStartEventArgs e)
+        {
+            XTrace.WriteLine("FrameLoadStart {0}", e.Url);
+        }
+
+        private void BtnGo_Click(Object sender, EventArgs e)
+        {
+            Browser.Load(txtUrl.Text);
+        }
+
+        private async void Web_FrameLoadEnd(Object sender, FrameLoadEndEventArgs e)
+        {
+            XTrace.WriteLine("FrameLoadEnd {0}", e.Url);
+
+            //一个网页会调用多次,需要手动自己处理逻辑
+            var url = e.Url;
+            var result = await Browser.GetSourceAsync();
+            var html = result;
+
+            ////调用js
+            //browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("alert('这是c#调用的js,给文本框赋值！')");
+            ////txtAccount
+            //browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("document.getElementById('kw').value='在C#里面给页面文本框进行赋值'");
+        }
+
+        private void WebBrowser1_DocumentCompleted(Object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.Host != Home.Host) return;
+
+            Text = e.Url + "";
+
+            XTrace.WriteLine(e.Url + "");
+        }
+
+        private void WebBrowser1_NewWindow(Object sender, CancelEventArgs e)
+        {
+            //var wb = (WebBrowser)sender;
+            //var newUrl = wb.Document.ActiveElement.GetAttribute("href");
+            //if (newUrl.IsNullOrEmpty()) return;
+
+            //wb.Url = new Uri(newUrl);
+
+            //e.Cancel = true;
+        }
+
+        private void WebBrowser1_Navigating(Object sender, WebBrowserNavigatingEventArgs e)
+        {
+            //XTrace.WriteLine("Navigating {0}", e.Url);
+        }
+
+        //private void Wb_BeforeScriptExecute(Object pDispWindow)
+        //{
+        //    var text = webBrowser1.DocumentText;
+        //    var vDocument = (IHTMLDocument2)webBrowser1.Document.DomDocument;
+
+        //    //var script = "alert('hello');";
+        //    //vDocument.parentWindow.execScript(script, "javascript");
+        //}
+
+        private void WebBrowser_BeforeNavigate2(Object pDisp, ref Object URL, ref Object Flags, ref Object TargetFrameName, ref Object postData, ref Object Headers, ref Boolean Cancel)
+        {
+            var uri = new Uri(URL + "");
+            if (uri.Host.EndsWithIgnoreCase("baidu.com"))
+            {
+                Cancel = true;
+                return;
+            }
+
+            XTrace.WriteLine("Navigate [{0}] {1}", TargetFrameName, URL);
+
+            if (postData != null)
+            {
+                var postDataText = System.Text.Encoding.ASCII.GetString(postData as Byte[]);
+                XTrace.WriteLine("POST {0}", postDataText);
+            }
+        }
+
+        //private void Wb_NewWindow3(ref Object ppDisp, ref Boolean Cancel, UInt32 dwFlags, String bstrUrlContext, String bstrUrl)
+        //{
+        //    XTrace.WriteLine("NewWindow3 [{0}] {1}", bstrUrlContext, bstrUrl);
+
+        //    Cancel = true;
+        //    webBrowser1.Navigate(bstrUrl);
+        //}
+    }
+}
